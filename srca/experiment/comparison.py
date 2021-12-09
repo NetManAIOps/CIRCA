9807acf480590f6f2cc37d33d38b59c2ff43ebfd
limjcst <limjcst@163.com>
2021-12-09 19:54:18 +0800

    feat: Compare baselines
    Signed-off-by: limjcst <limjcst@163.com>

---
+++ srca/experiment/comparison.py
@@ -0,0 +1,204 @@
+"""
+Compare algorithm combinations
+"""
+import csv
+from itertools import chain
+from itertools import product
+import logging
+from typing import Dict
+from typing import List
+from typing import Tuple
+
+from ..alg.base import GraphFactory
+from ..alg.base import Scorer
+from ..alg.common import EmptyGraphFactory
+from ..alg.common import Model
+from ..alg.common import NSigmaScorer
+from ..alg.common import evaluate
+from ..alg.correlation import CorrelationScorer
+from ..alg.correlation import PartialCorrelationScorer
+from ..alg.dfs import DFSScorer
+from ..alg.dfs import MicroHECLScorer
+from ..alg.graph.pcts import PCTSFactory
+from ..alg.graph.r import PCAlgFactory
+from ..alg.random_walk import RandomWalkScorer
+from ..alg.random_walk import SecondOrderRandomWalkScorer
+from ..model.case import Case
+
+
+_ALPHAS = (0.01, 0.05, 0.1, 0.5)
+_MAX_CONDS_DIMS = (2, 3, 5, 10, None)
+_TAU_MAXS = (0, 1, 2, 3)
+
+_ZERO_TO_ONE = (0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+_TRUE_AND_FALSE = (True, False)
+
+
+def _get_graph_factories(seed: int) -> Dict[str, GraphFactory]:
+    graph_factories: Dict[str, GraphFactory] = {}
+    for alpha, max_conds_dim in product(_ALPHAS, _MAX_CONDS_DIMS):
+        params = dict(alpha=alpha, seed=seed)
+        suffix = f"_a{alpha}"
+        if max_conds_dim is not None:
+            params["max_conds_dim"] = max_conds_dim
+            suffix += f"_m{max_conds_dim}"
+        graph_factories["PC_gauss" + suffix] = PCAlgFactory(method="PC-gauss", **params)
+        graph_factories["PC_gsq" + suffix] = PCAlgFactory(method="PC-gsq", **params)
+        for tau_max in _TAU_MAXS:
+            graph_name = "PCTS" + suffix + f"_t{tau_max}"
+            graph_factories[graph_name] = PCTSFactory(tau_max=tau_max, **params)
+    return graph_factories
+
+
+def _get_detectors(**scorer_params) -> Dict[str, Tuple[Scorer, float]]:
+    """
+    Map a detector name to a pair of Scorer and threshold
+    """
+    return {"NSigma": (NSigmaScorer(**scorer_params), 3)}
+
+
+def _get_anomaly_detection_models(**scorer_params) -> List[Model]:
+    detectors = _get_detectors(**scorer_params)
+    graph_factory = EmptyGraphFactory()
+    return [
+        Model(
+            graph_factory=graph_factory,
+            scorers=[
+                detector,
+            ],
+            names=("Empty", name),
+        )
+        for name, (detector, _) in detectors.items()
+    ]
+
+
+def _get_dfs_models(
+    graph_factories: Dict[str, GraphFactory], **scorer_params
+) -> List[Model]:
+    models: List[Model] = []
+    detectors = _get_detectors(**scorer_params)
+    # DFS
+    for detector_name, (detector, anomaly_threshold) in detectors.items():
+        scorer = DFSScorer(anomaly_threshold=anomaly_threshold, **scorer_params)
+        scorer_name = f"DFS_a{anomaly_threshold}"
+        for graph_name, graph_factory in graph_factories.items():
+            models.append(
+                Model(
+                    graph_factory=graph_factory,
+                    scorers=[detector, scorer],
+                    names=[graph_name, detector_name, scorer_name],
+                ),
+            )
+            # Microscope in ICSOC'18
+            models.append(
+                Model(
+                    graph_factory=graph_factory,
+                    scorers=[detector, scorer, CorrelationScorer(**scorer_params)],
+                    names=[graph_name, detector_name, scorer_name, "Pearson"],
+                ),
+            )
+    # MicroHECL
+    for detector_name, (detector, anomaly_threshold) in detectors.items():
+        scorers = {
+            f"MicroHECL_a{anomaly_threshold}_s{stop_threshold}": MicroHECLScorer(
+                anomaly_threshold=anomaly_threshold,
+                stop_threshold=stop_threshold,
+                **scorer_params,
+            )
+            for stop_threshold in _ZERO_TO_ONE
+        }
+        for graph_name, graph_factory in graph_factories.items():
+            for scorer_name, scorer in scorers.items():
+                models.append(
+                    Model(
+                        graph_factory=graph_factory,
+                        scorers=[detector, scorer],
+                        names=[graph_name, detector_name, scorer_name],
+                    ),
+                )
+    return models
+
+
+def _get_random_walk_models(
+    graph_factories: Dict[str, GraphFactory], **scorer_params
+) -> List[Model]:
+    models: List[Model] = []
+    for rho, remove_sla in product(_ZERO_TO_ONE, _TRUE_AND_FALSE):
+        params = dict(rho=rho, remove_sla=remove_sla, **scorer_params)
+        suffix = f"_r{rho}"
+        if remove_sla:
+            suffix += "_nosla"
+        for graph_name, graph_factory in graph_factories.items():
+            models.append(
+                Model(
+                    graph_factory=graph_factory,
+                    scorers=[
+                        CorrelationScorer(**scorer_params),
+                        RandomWalkScorer(**params),
+                    ],
+                    names=[graph_name, "Pearson", "RW" + suffix],
+                ),
+            )
+            for beta in _ZERO_TO_ONE:
+                models.append(
+                    Model(
+                        graph_factory=graph_factory,
+                        scorers=[
+                            PartialCorrelationScorer(**scorer_params),
+                            SecondOrderRandomWalkScorer(beta=beta, **params),
+                        ],
+                        names=[
+                            graph_name,
+                            "PartialCorrelation",
+                            "RW_2" + suffix + f"_b{beta}",
+                        ],
+                    ),
+                )
+    return models
+
+
+def get_models(seed: int = 519) -> List[Model]:
+    """
+    Prepare algorithm candidates
+    """
+    scorer_params = dict(seed=seed)
+    graph_factories = _get_graph_factories(seed=seed)
+    models = list(
+        chain(
+            _get_anomaly_detection_models(**scorer_params),
+            _get_dfs_models(graph_factories=graph_factories, **scorer_params),
+            _get_random_walk_models(graph_factories=graph_factories, **scorer_params),
+        )
+    )
+
+    return models
+
+
+def run(
+    models: List[Model],
+    cases: List[Case],
+    output_dir: str = "output",
+    report_filename: str = "report.csv",
+):
+    """
+    Compare different models
+    """
+    logger = logging.getLogger(run.__module__)
+    scores: List[Tuple[str, float, float, float, float]] = []
+    for model in models:
+        name = model.name
+        logger.info("Evaluate %s", name)
+        report = evaluate(model, cases, output_dir=output_dir)
+        item = (
+            name,
+            report.accuracy(1),
+            report.accuracy(3),
+            report.accuracy(5),
+            report.average(5),
+        )
+        scores.append(item)
+
+    with open(report_filename, "w", encoding="UTF-8") as obj:
+        writer = csv.writer(obj)
+        writer.writerow(["method", "AC@1", "AC@3", "AC@5", "Avg@5"])
+        writer.writerows(scores)
